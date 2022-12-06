102b289f144007eb9f521d11d8fca5bbd0d4f905
samyogita <77333607+samyogita@users.noreply.github.com>
2022-12-06 22:14:10 +0545

    B - LOOKUP
    You are given strings S and T consisting of lowercase English letters. Determine whether T is a (contiguous) substring of S.
    
    A string Y is said to be a (contiguous) substring of X if and only if Y can be obtained by performing the operation below on X zero or more times.
    
    Do one of the following.
    Delete the first character in X.
    Delete the last character in X.
    For instance, tag is a (contiguous) substring of voltage, while ace is not a (contiguous) substring of atcoder.
    
    Constraints
    S and T consist of lowercase English letters.
    1≤∣S∣,∣T∣≤100 (∣X∣ denotes the length of a string X.)

--- atcodeb.py
+++ atcodeb.py
@@ -1,17 +1,7 @@
-h, w = list(map(int, input().split()))
-matrix = []
-res = [0] * w
-for i in range(h):		 
-    s = input()
-    cur = []
-    for i in s:	 
-        cur.append(1 if i == '#' else 0)
-    matrix.append(cur)
-
-for i in range(w):
-    for j in range(h):
-        res[i] += matrix[j][i]
-for ele in res:
-    print(ele, end = ' ')
-        
-       
+s = input()
+t = input()
+ans = s.find(t)
+if ans == -1:
+    print('No')
+else:
+    print('Yes')
